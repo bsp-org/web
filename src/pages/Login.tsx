87d0a615920bf7911488ebff4ae2d5b8cce2f862
Luci Furtun <lucianfurtun@gmail.com>
2025-12-29 00:03:15 +0200

    Improve login view

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,11 +5,11 @@ const Login = () => {
     const {
         register,
         handleSubmit,
+        formState: { isDirty },
       } = useForm();
     
       return (
-        <div className='flex flex-col gap-4 justify-center items-center h-screen max-w-xl mx-auto p-4'>
-                
+        <div className='flex flex-col gap-4 justify-center items-center max-w-xl mx-auto p-4'>
                 <form onSubmit={handleSubmit((data) => console.log(data))} className="flex flex-col gap-4 border-2 border-gray-300 rounded-md p-4 w-md">
                 <div className="flex flex-col gap-4 items-center">
                     <h1 className='text-2xl font-bold'>Login</h1>
@@ -18,8 +18,7 @@ const Login = () => {
                         <Input {...register('email')} type='email' placeholder='Email' className='text-lg' />
                     </div>
                     <div className="flex gap-4 items-center justify-center">
-                        <Button>Login</Button>
-                        <Button variant='secondary'>Register</Button>
+                        <Button className="cursor-pointer" disabled={!isDirty}>Login</Button>
                     </div>
                 </form>
         </div>
